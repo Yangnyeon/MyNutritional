e0670a927f6638ccaeb290797c03ce9939ac7cf0
YangNyeon <tmdgurdla324@naver.com>
2024-01-18 18:44:06 +0900

    1.네비게이션바 추가 / 프래그먼트 추가

--- app/src/main/java/com/example/nutritionalrecom/MainActivity.kt
+++ app/src/main/java/com/example/nutritionalrecom/MainActivity.kt
@@ -2,12 +2,53 @@ package com.example.nutritionalrecom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import com.example.nutritionalrecom.databinding.ActivityMainBinding
+import com.example.nutritionalrecom.nutCommunity.nutCommunityFragment
+import com.example.nutritionalrecom.nutSleep.nutSleepFragment
+import com.example.nutritionalrecom.nutTest.nutTestFragment
+import com.example.nutritionalrecom.nutTypes.nutTypesFragment
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+    val fragment = nutTypesFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /*setContentView(R.layout.activity_main)*/
 
-        //하이ㅇㄹㅇㄹ
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+
+        mBinding!!.bottonavi.setItemSelected(R.id.first)
+
+        mBinding!!.bottonavi.setOnItemSelectedListener {
+            when (it) {
+                R.id.first -> {
+                    val MainFragment1 = nutTypesFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment1).commit()
+                }
+                R.id.second -> {
+                    val MainFragment2 = nutCommunityFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment2).commit()
+                }
+                R.id.third -> {
+                    val MainFragment3 = nutTestFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment3).commit()
+                }
+                R.id.four -> {
+                    val MainFragment4 = nutSleepFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment4).commit()
+                }
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
