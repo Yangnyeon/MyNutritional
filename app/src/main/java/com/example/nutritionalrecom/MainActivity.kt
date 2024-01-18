6936251096a2fda7ae47228223e53af31d654bbb
YangNyeon <tmdgurdla324@naver.com>
2024-01-18 15:36:16 +0900

    01/18 커밋

--- app/src/main/java/com/example/nutritionalrecom/MainActivity.kt
+++ app/src/main/java/com/example/nutritionalrecom/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //hahahaㅇㄹㅇㄴㄹㅇㄴㄹㅁㅇㄴㄹ민어어어엄
+        //풀 테스트
     }
 }
